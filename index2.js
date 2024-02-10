73953e6b4e652399321e10408dded9f85a26fae8
gamenarkyt <gagaritin11@gmail.com>
2024-02-11 01:17:52 +0300

    new version

---
+++ index2.js
@@ -0,0 +1,28 @@
+const bookmarks = document.getElementsByClassName("bookmarks")[0].children
+
+const links = {
+	piped: "https://piped.video/feed/",
+	github: "https://github.com/",
+}
+
+for (let i = 0; i < bookmarks.length; i++) {
+	const classname = bookmarks[i].className
+	const linkname = classname.match("site-(.*)")[1]
+	const element = document.getElementsByClassName(classname)[0]
+
+	// onclick
+	element.addEventListener("click", () => {
+		if (links[linkname]) {
+			window.open(links[linkname], "_self")
+		} else [console.log("bookmark not found")]
+	})
+
+	// onmiddleclick
+	element.addEventListener("mouseup", (event) => {
+		if (event.button === 1) {
+			if (links[linkname]) {
+				window.open(links[linkname])
+			} else [console.log("bookmark not found")]
+		}
+	})
+}
